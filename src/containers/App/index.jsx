f1e2ba43fefea690f46d6a7c3d426c176d35f71c
unknown <Admin@Seanergy96.SeanergyInd.in>
2019-11-25 18:38:05 +0530

    Added WithdrawlHistoryPage

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import style from './style.scss';
-import EmployeeMaintenance from '../../components/Employees/EmployeeMaintenance';
-import ValuationMaintenance from '../../components/Valuation/ValuationMaintenance';
+// import EmployeeMaintenance from '../../components/Employees/EmployeeMaintenance';
+// import ValuationMaintenance from '../../components/Valuation/ValuationMaintenance';
+import WithdrawalsHistory from '../../components/Withdrawals/WithdrawalsHistory';
 
 class App extends React.PureComponent {
   render() {
     return (
       <div className={style.mainBody}>
-        <EmployeeMaintenance />
-        <ValuationMaintenance />
+        {/* <EmployeeMaintenance />
+        <ValuationMaintenance /> */}
+        <WithdrawalsHistory />
       </div>
     );
   }
